ab56db2ef35cdd40a8fc8d0458b44fcbd68da3e5
Clinton Cario <clc184@pitt.edu>
2012-08-15 17:09:33 -0400

    first skeleton working versing version of rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorWebsite::Application.config.secret_token = '7ed7a11bfadd5214663910f71a28f186178f0e47edb63b7bf949d4f774606174fab2b95b92ff79f0c65fc538c2f294dfee8afb6ba080bc206a39e9865238010d'

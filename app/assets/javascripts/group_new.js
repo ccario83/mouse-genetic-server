7a212e720454f5b050951616d49b4da236dc46a8
Clinton Cario <clc184@pitt.edu>
2013-03-21 15:01:14 -0400

    Changed groups/new to have searchable list instead of displaying all users

--- app/assets/javascripts/group_new.js
+++ app/assets/javascripts/group_new.js
@@ -13,40 +13,52 @@ $(window).bind("load", function()
 		var userIds = $.map($(".ui-selected"), function(n, i){ return parseInt(n.id); });
 		// Add their IDs to the group_users input ID, which was created by rails and simple_form to handle the submitted user list
 		$('#group_users').val(userIds);	
+		
+		// Rehide unselected users (REMOVE FOR PAGINATION)
+		$('#selectable-users li').hide();
+		$(".ui-selected").show();
 	});
+	
 	//The div is initially hidden to prevent the full list from being shown before pagination. Pagination is done rendering now, so show the list
 	$('#selectable-users').show(); 
 
 	// Populate the group_user list with any values sent from the server (found in the #preselected-users hidden input in the multi_select partial)
 	$('#group_users').val($('#preselected-users').val());
 	
+	
+	// Hide all users (REMOVE FOR PAGINATION)
+	$('#selectable-users li').hide();
+	
 	// Style the preselected users so that they appear selected ot the end-user
 	var userIds = jQuery.parseJSON($('#preselected-users').val());
 	for (var i = 0; i< userIds.length; i++)
 	{
-		$('#selectable-users li#'+userIds[i]).addClass("ui-selected");
+		$('#selectable-users li#'+userIds[i]).addClass("ui-selected").show;
 	}
 	
-	/*
+	
 	var user_list=[];
-	var users = $('.ui-selectee > div > a')
+	var users = $('.ui-selectee > div > a');
 	for (var i = 0; i < users.length; i++)
-		var term = node.data.title + "  [ID=" + node.data.key + "]";
-		list.push({ 'label': term, 'value': node.data.key});
-	make_autocomplete_list(mpath_first_node, user_list);
-	$('#mpath_search').autocomplete(
+		user_list.push({'value': i, 'label': users[i].text});
+	$('#user_search').autocomplete(
 	{ 
-		source: mpath_term_list, 
+		source: user_list, 
 		minLength: 3, 
 		select: function(event, ui)
-		{ 
-			$('#mpath_tree').dynatree('getTree').activateKey(ui.item.value);
-			$('#mpath_search').val(ui.item.label);
+		{
+			var user_container = $(users[ui.item.value]).parent().parent();
+			user_container.show();
+			// First style the element so the end-user knows their click was registered
+			user_container.toggleClass("ui-selected");
+			// Get all selected users by ID
+			var userIds = $.map($(".ui-selected"), function(n, i){ return parseInt(n.id); });
+			// Add their IDs to the group_users input ID, which was created by rails and simple_form to handle the submitted user list
+			$('#group_users').val(userIds);	
 			return false; // Return false to cancel the event, which prevents jQuery from replacing the field with the 'value' (which is just the ID)
 		}
 	});
-	*/
+
 
 });
-
 
